389194d5c3593ec94e74ee4cb70aed0ae50f4116
Burakzdd <burak67ozd@outlook.com>
2022-05-14 16:00:28 +0300

    Modified Binary Segmentation

--- Binary_Segmentation/test.py
+++ Binary_Segmentation/test.py
@@ -30,7 +30,7 @@ for img in os.listdir(image_path):
     image = Image.open(image_path+img)
     image = image.resize((256,256), resample=Image.NEAREST)
     image = transforms.ToTensor()(image)
-    image = transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))(image)
+    # image = transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))(image)
     image = image.unsqueeze(0)
     image = image.to(device, dtype=torch.float32)
 
@@ -44,29 +44,6 @@ for img in os.listdir(image_path):
     h, w = image.shape[:2]
     mask_image = cv2.resize(mask_image, (w, h))
   
-    # prediction = prediction.permute(1, 2, 0).cpu().numpy()
-    print(mask_image)
-    mask_image *=255
-    mask_image = cv2.normalize(mask_image, mask_image, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)  # value of mask pixels may be 255
-
-    for y in range(mask_image.shape[0]):
-        for x in range(mask_image.shape[1]):
-            if mask_image[y][x] < 0.5:
-                print(mask_image[y][x])
-                mask_image[y][x] = 0
-            else:
-                print(mask_image[y][x])
-
-                mask_image[y][x] = 255
-
-    contours = cv2.findContours(
-        mask_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
-    for cntr in contours:
-        x, y, w, h = cv2.boundingRect(cntr)
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
-
-
     cv2.imshow("image", image)
     cv2.imshow("mask", mask_image)
 
